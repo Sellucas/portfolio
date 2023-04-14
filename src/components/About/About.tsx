9a345fc59cfe470b823c350a7db0692b0c4c3c49
Sellucas <lucassellmachado@gmail.com>
2023-04-14 17:26:54 -0300

    feat: about me section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,7 +5,45 @@ const About = () => {
   return (
     <div style={{ backgroundColor: "white", color: "rgba(0, 0, 0, 0.8)" }}>
       <div className={scss.page}>
-        <h3>ABOUT ME</h3>
+        <h3 className={scss.title}>ABOUT ME</h3>
+
+        <div className={scss.about}>
+          <div>
+            <p id={scss.firstP}>
+              I believe that the best user experiences come from designs that
+              are both <span>beautiful and functional</span>. That's why I
+              always prioritize user-centered design principles and focus on
+              creating intuitive interfaces that are easy to use.
+            </p>
+
+            <p>
+              Throughout my experience as a Front-end Developer, I've learned
+              the importance of <span>collaboration and communication</span>{" "}
+              within a team. I'm a strong believer in open communication and
+              always strive to keep everyone on the same page throughout the
+              development process.
+            </p>
+          </div>
+
+          <div>
+            <img src="" alt="/" />
+          </div>
+        </div>
+
+        <div className={scss.technologies}>
+          <p>
+            Here area a few <span>technologies</span> I've been working with
+            recently:
+          </p>
+          <ul>
+            <li>JavaScript</li>
+            <li>TypeScript</li>
+            <li>ReactJs</li>
+            <li>Tailwind</li>
+            <li>Sass</li>
+            <li>Bootstrap</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
