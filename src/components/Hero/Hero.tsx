bfe00e425b100cb899fd7fbd6d5eadede358449f
Sellucas <lucassellmachado@gmail.com>
2023-05-05 14:24:36 -0300

    chore: add react-i18next translation lib

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,24 +1,35 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import scss from "./Hero.module.scss";
 import { Reveal } from "../utils/Reveal";
+import { useTranslation } from "react-i18next";
 
 const Hero: React.FC = () => {
+  const {
+    t,
+    i18n: { changeLanguage, language },
+  } = useTranslation();
+  const [currentLanguage, setCurrentLanguage] = useState(language);
+
+  const hadleCurrentLanguage = () => {
+    const newLanguage = currentLanguage === "en" ? "pt" : "en";
+    changeLanguage(newLanguage);
+    setCurrentLanguage(newLanguage);
+  };
+
   return (
     <section className={scss.page}>
       <div className={scss.container}>
         <div>
           <Reveal delay={0.2}>
-            <h2>Hi, my name is</h2>
+            <h2>{t("heroTitleIntro")}</h2>
           </Reveal>
           <h1>
             <Reveal delay={0.4}>
-              <p>
-                Lucas Sell Machado. <br />
-              </p>
+              <p>Lucas Sell Machado.</p>
             </Reveal>
             <Reveal delay={0.6}>
               <p>
-                I'm a <span>FRONTEND DEVELOPER</span>.
+                {t("heroTitleStack")} <span>FRONTEND DEVELOPER</span>.
               </p>
             </Reveal>
           </h1>
@@ -26,14 +37,11 @@ const Hero: React.FC = () => {
 
         <div>
           <Reveal delay={0.8}>
-            <p className={scss.description}>
-              My specialty as a front-end developer is creating smooth and
-              intuitive user experiences. I'm excellent at utilizing the latest
-              technologies to deliver top-notch web applications. I have
-              extensive knowledge in Front-End, which allows me to easily build
-              complex and dynamic applications.
-            </p>
+            <p className={scss.description}>{t("heroDescription")}</p>
           </Reveal>
+          <button type="button" onClick={hadleCurrentLanguage}>
+            Change Language
+          </button>
         </div>
       </div>
     </section>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import Layout from "@/components/Layout";
 import "@/styles/global.css";
 import type { AppProps } from "next/app";
 
+import "../lib/i18n";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
