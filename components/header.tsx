5fb2b521ba08cc7acdf3c67d5419661064eca36c
Lucas Sell <lucassellmachado@gmail.com>
2024-04-23 14:26:56 -0300

    feat: add logo

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import { MenuNavbar } from "@/components/menu-navbar";
 export const Header = () => {
   return (
     <header className="border-b-2">
-      <div className="max-w-[1575px] mx-auto flex justify-between items-center p-2">
+      <div className="max-w-[1575px] mx-auto flex justify-between items-center px-2 h-14">
         <Logo />
         <MenuNavbar />
       </div>
--- components/logo.tsx
+++ components/logo.tsx
@@ -1,5 +1,12 @@
+import { Sacramento } from "next/font/google";
 import Link from "next/link";
 
+const font = Sacramento({ subsets: ["latin"], weight: ["400"] });
+
 export const Logo = () => {
-  return <Link href={"/"}>Sellucas</Link>;
+  return (
+    <Link className={`${font.className} text-4xl`} href={"/"}>
+      Sellucas
+    </Link>
+  );
 };
