fd73caaf658909ce5e100db6a8cb997829ef89f5
Lucas Sell <lucassellmachado@gmail.com>
2024-04-29 13:46:50 -0300

    refactor: logo update

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,11 +1,11 @@
-import { Sacramento } from "next/font/google";
+import { Macondo_Swash_Caps } from "next/font/google";
 import Link from "next/link";
 
-const font = Sacramento({ subsets: ["latin"], weight: ["400"] });
+const font = Macondo_Swash_Caps({ subsets: ["latin"], weight: ["400"] });
 
 export const Logo = () => {
   return (
-    <Link className={`${font.className} text-4xl text-foreground`} href={"/"}>
+    <Link className={`${font.className} text-3xl text-foreground`} href={"/"}>
       Sellucas
     </Link>
   );
